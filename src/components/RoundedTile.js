86dc10bf9107ce87ebaf1c73332b780b62cf2c64
Pedro Lima e Silva <pedrocaslsilva403@gmail.com>
2023-11-15 18:19:52 +0000

    Add Smooth Scale to Tile

--- src/components/RoundedTile.js
+++ src/components/RoundedTile.js
@@ -10,7 +10,18 @@ export class RoundedTile extends Tile {
     this.shader.radius = 10;
   }
 
+  setItem(item) {
+    super.setItem(item);
+    this.shader.progress = item.progress;
+  }
+
   _focus() {
     this.color = 0xffff00ff;
+    this.setSmooth("scale", 1.05);
+  }
+
+  _unfocus() {
+    super._unfocus();
+    this.setSmooth("scale", 1);
   }
 }
